51a1600ed9640bb91fd0deda8e02e7fa62f8cc76
Matt Farmer <mrfarmer777@gmail.com>
2018-06-29 05:49:14 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,45 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+  end
 
+  get "/posts/new" do
+    erb :new
+  end
+
+  post "/posts" do
+    #! Create method is part of ActiveRecord already, silly!
+    Post.create(params)
+    redirect '/posts'
+  end
+
+  get "/posts" do
+    #? Does active record have its own all method or is the one we made?
+    @posts=Post.all
+    erb :index
+  end
+
+  get "/posts/:id" do
+    #? Is it better to do this here or in the ERB?
+    @post=Post.find(params[:id])
+    erb :show
+  end
+
+  get "/posts/:id/edit" do
+    @post=Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post=Post.find(params[:id])
+    @post.name=params[:name]
+    @post.content=params[:content]
+    @post.save!
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do
+    Post.destroy(params[:id])
+    erb :delete
   end
 end
