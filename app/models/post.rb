fdb97dfd46a47d9f9b2814ad4ddf37f20f3397c8
Matt Farmer <mrfarmer777@gmail.com>
2018-06-29 02:39:31 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,4 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
-  attr_accessor :name, :content
-
-  POSTS = []
-  def initialize(opts)
-    @name=opts[:name]
-    @content=opts[:content]
-  end
-
-
 end
